0c77d6d80f491837959f5adcaf313ecd587d5bd9
Shade Webb <shadew@tutanota.com>
2019-03-27 20:05:38 -0400

    addTodo method add objects to array

--- todo.js
+++ todo.js
@@ -1,12 +1,17 @@
-~"use strict";
+"use strict";
+
+
 
 var todoList = {
-    todos: ['item 1', 'item 2', 'item 3'],
+    todos: [],
     displayTodos: function () {
         console.log('My Todos: ', this.todos);
     },
-    addTodo: function (todo) {
-        this.todos.push(todo);
+    addTodo: function (todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
         this.displayTodos();
     },
     changeTodo: function (position, newValue) {
