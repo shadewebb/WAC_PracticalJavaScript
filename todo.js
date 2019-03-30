c807b085b8c1865c2d6839a53ee1b88e36d03200
Shade Webb <shadew@tutanota.com>
2019-03-29 22:49:19 -0400

    V11 finished but delete button eventListener does not function?

--- todo.js
+++ todo.js
@@ -74,10 +74,10 @@ var view = {
     displayTodos: function () {
         var todosUl = document.querySelector('ul');
         todosUl.innerHTML = '';
-        for (var i = 0; i < todoList.todos.length; i++) {
+
+        todoList.todos.forEach(function (todo, position) {
             var todoLi = document.createElement('li');
             var todoTextWithCompletion = '';
-            var todo = todoList.todos[i];
 
             if (todo.completed === true) {
                 todoTextWithCompletion = '(x) ' + todo.todoText;
@@ -85,11 +85,11 @@ var view = {
                 todoTextWithCompletion = '( ) ' + todo.todoText;
             }
 
-            todoLi.id = i;
+            todoLi.id = position;
             todoLi.textContent = todoTextWithCompletion;
             todoLi.appendChild(this.createDeleteButton());
             todosUl.appendChild(todoLi);
-        }
+        }, this);
     },
     createDeleteButton: function () {
         var deleteButton = document.createElement('button');
